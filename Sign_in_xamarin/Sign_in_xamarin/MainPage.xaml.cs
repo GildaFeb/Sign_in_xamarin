7f99d3dda3daea95cdae64d42b5e9a1c7ba3a6ab
Gilda Feb D. Lacanienta <gildafebl@gmail.com>
2021-03-20 00:45:30 +0800

    Add project files.

---
+++ Sign_in_xamarin/Sign_in_xamarin/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Sign_in_xamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LogInBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
